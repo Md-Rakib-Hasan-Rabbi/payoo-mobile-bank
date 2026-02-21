1175f1cfa66783ded240d4f3d88c91d87c58b347
Rakib <rakibmcpsc339@gmail.com>
2026-02-21 14:48:19 +0600

    make login page interactive

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,14 @@
-console.log("Login page script loaded");
+const button = document.getElementById("login-btn").addEventListener("click", function(){
+    const number = document.getElementById("input-number").value;
+    console.log(number);
+
+    const pin = document.getElementById("input-pin").value;
+    console.log(pin);
+
+    if(number === "01234567890" && pin === "1234"){
+        alert("Login successful!");
+        window.location.assign="./home.html";
+    } else {
+        alert("Invalid number or PIN. Please try again.");
+    }
+})

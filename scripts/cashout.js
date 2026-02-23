bf87bc030798a901c9c5532cf03254a758730f73
Rakib <rakibmcpsc339@gmail.com>
2026-02-23 22:25:03 +0600

    Cretae a fuction to optimize the code

--- scripts/cashout.js
+++ scripts/cashout.js
@@ -1,37 +1,25 @@
-document.getElementById("cashout-btn").addEventListener("click", function(){
-    const agentNumber = document.getElementById("cashout-number").value;
-    if(agentNumber.length !== 11){
-        alert("Invalid number. Please enter an 11-digit number.");
-        return;
-    }
-    console.log(agentNumber);
+document.getElementById("cashout-btn").addEventListener("click", function () {
+  const agentNumber = getValue("cashout-number");
+  if (agentNumber.length !== 11) {
+    alert("Invalid number. Please enter an 11-digit number.");
+    return;
+  }
+  const amount = getValue("cashout-amount");
+  const pin = document.getElementById("cashout-pin").value;
+  const balance = document.getElementById("balance").innerText;
 
-    const amount = document.getElementById("cashout-amount").value;
-    console.log(amount);
+  const newbalance = Number(balance) - Number(amount);
+  if (newbalance < 0) {
+    alert("Insufficient balance. Please try again.");
+  }
 
-    const balance = document.getElementById("balance").innerText;
-    console.log(balance);
+  if (pin === "1234") {
+    alert("Cashout Successful!");
+    document.getElementById("balance").innerText = newbalance;
+  } else {
+    alert("Invalid PIN. Please try again.");
+    return;
+  }
+});
 
-    const pin = document.getElementById("cashout-pin").value;
-    console.log(pin);
 
-    const newbalance = Number(balance) - Number(amount);
-    
-
-    if(newbalance < 0){
-        alert("Insufficient balance. Please try again.");
-    }
-    
-
-    if(pin === "1234"){
-        alert("Cashout Successful!");
-        document.getElementById("balance").innerText = newbalance;
-        console.log(newbalance);
-
-    }
-    else{
-        alert("Invalid PIN. Please try again.");
-        return;
-    }
-
-    })
---
+++ scripts/machine.js
@@ -0,0 +1,6 @@
+function getValue(id){
+    const value = document.getElementById(id).value;
+    console.log(id, value);
+    return value;
+
+}
